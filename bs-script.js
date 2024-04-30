f8ff3a03c76948d7b9c1080812aa0ec6148ac4f3
Donny Zhao <simon861102@gmail.com>
2024-04-30 23:19:06 +1000

    clear

--- bs-script.js
+++
@@ -1,8 +0,0 @@
-// bs-script.js
-(function() {
-    var script = document.createElement('script');
-    script.async = true;
-    script.src = 'http://localhost:3000/browser-sync/browser-sync-client.js?v=2.27.7';
-    document.body.appendChild(script);
-})();
-
