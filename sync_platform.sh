18866e912863247d4a3ed6020d54e9151cd4710e
Donny Zhao <simon861102@gmail.com>
2024-11-11 21:09:40 +1100

    local

--- sync_platform.sh
+++ sync_platform.sh
@@ -2,7 +2,7 @@
 
 # rsync -avL --delete --progress -e "ssh -i ~/.ssh/Linux-CodeCambat.pem"  --exclude "vendor" --exclude "node_modules/*"  --exclude "storage" ~/git/kalepa/*  ec2-user@platform.datap.ai:/var/www/kalepa/
 # rsync -avL --delete --progress -e "ssh -i ~/.ssh/test_datapai.pem"  --exclude "node_modules/*" --exclude "vendor" --exclude "storage" ~/git/kalepa/*  ec2-user@test.datap.ai:/var/www/kalepa/
-rsync -avL --delete --progress -e "ssh -i ~/.ssh/test_datapai.pem"  --exclude "node_modules/*" --exclude "vendor" --exclude "storage" ~/git/kalepa/*  ec2-user@13.55.219.134:/var/www/kalepa/
+rsync -avL --delete --progress -e "ssh -i ~/.ssh/Linux-CodeCambat.pem"  --exclude "node_modules/*" --exclude "vendor" --exclude "storage" ~/git/kalepa/*  ec2-user@13.55.219.134:/var/www/kalepa/
 
 
 #npm run watch-poll 
